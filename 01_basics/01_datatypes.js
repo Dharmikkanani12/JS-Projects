443f06b7ca3b08e47f7cab621350a2e96bed610c
Dharmikkanani12 <152086922+Dharmikkanani12@users.noreply.github.com>
2024-01-31 19:10:04 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // trat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+3);
+
+console.log("Dharmik");
+
+let name = "Dharmik" // string datatypes
+let age = 18 //  number datatypes
+let isLoggenIn = true // boolean datatypes
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
+
+/* Types of datatypes : -
+
+ // number => 2 to  power 53
+ // bigint
+ // string => ""
+ // boolean => true or false
+ // null => standalone value
+ // undefined => not assigned value
+ // symbol => unique
+
+ // object - ex. null 
+
+
+ */
