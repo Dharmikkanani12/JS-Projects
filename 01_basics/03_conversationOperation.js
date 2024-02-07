0d592eb2fd7a91e1695029e15f1a91a3b6a4d580
Dharmikkanani12 <152086922+Dharmikkanani12@users.noreply.github.com>
2024-02-07 15:17:39 +0000

    cofig done

--- 01_basics/03_conversationOperation.js
+++ 01_basics/03_conversationOperation.js
@@ -24,5 +24,46 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+/* 
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+*/
+
+
+
+// *********** Operations *********//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2**9);
+// console.log(2*25);
+// console.log(2%3);
+
+let str1 = 25
+let str2 = 35
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +2 / 2);
+// console.log(1 + 2 + "gm");
+// console.log("1" + "2" + "25") ;
+
+//console.log((3-25) * (3/5));
+
+//console.log(true + false); => not preferable //
+
+
+
+let gameCounter = 25
+gameCounter++;
+console.log(gameCounter);
